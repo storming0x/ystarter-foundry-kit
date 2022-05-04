56a636b1ae86b728f8578b2c416e818488e78c8e
storming0x <storm0x@storm0x.com>
2022-05-03 23:43:48 -0600

    fix: linter

--- src/Sugar4626.sol
+++ src/Sugar4626.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
--- src/VaultWrapper.sol
+++ src/VaultWrapper.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
